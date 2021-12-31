593b20e13e12d481e03b4e2af5ead0309c458d3c
newtonkim <knewts98@gmail.com>
2021-12-31 15:53:29 +0300

    add summarised MakeHttp function

--- app/Http/Controllers/payment/mpesa/MpesaController.php
+++ app/Http/Controllers/payment/mpesa/MpesaController.php
@@ -22,10 +22,30 @@ class MpesaController extends Controller
                 CURLOPT_HEADER => false,
                 CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET')
             )
-            );
-        $response = (curl_exec($curl));
+        );
+        $response = curl_exec($curl);
         curl_close($curl);
 
         return $response;
+    }
+
+    public function MakeHttp($url, $body)
+    {
+        $curl = curl_init();
+        curl_setopt_array(
+            $curl,
+            array(
+                    CURLOPT_URL              => $url,
+                    CURLOPT_HTTPHEADER => array('Content-Type:application/json', 'Authorization:Bearer '. $this->getAccessToken()),
+                    CURLOPT_RETURNTRANSFER   => true,
+                    CURLOPT_POST             =>  true,
+                    CURLOPT_POSTFIELDS       => json_encode($body)
+                )
+            );
+
+        $curl_response = curl_exec($curl);
+        curl_close($curl);
+
+        return $curl_response;
     }
 }
