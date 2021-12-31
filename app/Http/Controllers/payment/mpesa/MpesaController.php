025198b231b0aa0a9bf05209ffedd9bfaec4e32a
newtonkim <knewts98@gmail.com>
2021-12-31 14:15:31 +0300

    introduce curl and mpesa sandbox

--- app/Http/Controllers/payment/mpesa/MpesaController.php
+++ app/Http/Controllers/payment/mpesa/MpesaController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class MpesaController extends Controller
 {
-    //
+    public function getAccessToken()
+    {
+        $url = env('MPESA_ENV') == 0
+        ? 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+        : 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+        $curl =curl_init($url);
+        curl_setopt_array(
+            $curl,
+            array(
+                CURLOPT_HTTPHEADER => ['Content-Type: application/json; charset=utf8'],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER => false,
+                CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET')
+            )
+            );
+        $response = (curl_exec($curl));
+        curl_close($curl);
+
+        return $response;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\payment\mpesa\MpesaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('get-token', [MpesaController::class, 'getAccessToken']);
