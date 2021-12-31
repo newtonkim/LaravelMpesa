3e529cc46a4b8b40b956906f41a94c25374ed601
newtonkim <knewts98@gmail.com>
2021-12-31 10:54:18 +0300

    add mpesa ui and controller

---
+++ app/Http/Controllers/payment/mpesa/MpesaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\payment\mpesa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MpesaController extends Controller
+{
+    //
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,56 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Mpesa Integration </title>
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel Daraja</title>
 </head>
 
 <body>
     <div class="container">
-        <h1>Laravel Mpesa Integration</h1>
+
+        <div class="row mt-5">
+            <div class="col-sm-8 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        Obtain Access Token
+                    </div>
+                    <div class="card-body">
+                        <h4 id="access_token"></h4>
+                        <button id="getAccessToken" class="btn btn-primary">Request Access Token</button>
+                    </div>
+                </div>
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Register URL
+                    </div>
+                    <div class="card-body">
+                        <button class="btn btn-primary">Register URLs</button>
+                    </div>
+                </div>
+
+                <div class="card mt-5">
+                    <div class="card-header">Simulate Transactions</div>
+                    <div class="card-body">
+                        <form action="">
+                            @csrf
+                            <div class="form-group">
+                                <label for="amount">Amount</label>
+                                <input type="number" name="amount"  class="form-control" id="amount">
+                            </div>
+                            <div class="form-group">
+                                <label for="account">Account</label>
+                                <input type="text" name="account"  class="form-control" id="account">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </body>
 
